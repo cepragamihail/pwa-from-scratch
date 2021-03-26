949e477f4f88bf7dab8798eb1edffd91a15f6e98
m.cepraga <m.cepraga@viamedici.de>
2021-03-26 11:50:43 +0200

    workbox dynamic routing

--- src-sw.js
+++ src-sw.js
@@ -1,3 +1,20 @@
 import {precacheAndRoute} from 'workbox-precaching';
+import {registerRoute} from 'workbox-routing';
+import {CacheFirst} from 'workbox-strategies';
+import {ExpirationPlugin} from 'workbox-expiration';
 
 precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+    new RegExp('https:\/\/pluralsight\-pwa\-scratch\.firebaseio\.com\/.*\.json'),
+    new CacheFirst({
+      cacheName: "api-cache",
+      plugins: [
+        new ExpirationPlugin({
+          maxEntries: 50,
+          purgeOnQuotaError: true,
+          maxAgeSeconds: 10 * 86400 // 10 days
+        })
+      ]
+    })
+);
